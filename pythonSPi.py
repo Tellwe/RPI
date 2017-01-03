9db0f85b495a85951f2346ecfc770285570e1828
Tellwe <sebastian.tellwe@gmail.com>
2017-01-03 11:01:53 +0000

    Code uppdated with comments

--- pythonSPi.py
+++ pythonSPi.py
@@ -6,79 +6,141 @@ import libbcm2835._bcm2835 as soc
 #include <stdio.h>
 
 from ctypes import *
-#include "MRF89XA.h"
+from bluetooth import *
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+
 trRESET = soc.RPI_V2_GPIO_P1_15 	
 trCSCON = 26
 trCSDATA = soc.RPI_V2_GPIO_P1_13 
 #inputs
 trIRQ0 =13 
 trIRQ1 = soc.RPI_V2_GPIO_P1_11 
-
-#void WriteSPI(unsigned char);
-#unsigned char ReadSPI(void);
-
-#void TransiverInitReceiving(void); 
-#void TransiverInitTransmitter(void);
-#void TransiverInit(void);
-#void WriteControlRegTransiver(unsigned char , unsigned char);
-
-#unsigned char RegisterRead(unsigned char);
-#void RegisterSet(unsigned char address, unsigned char value);
-#unsigned char ReadFIFO(void);
-#void WriteFIFO(unsigned char);
-#void TransmittString(unsigned char dataString[]);
+#*************************************************************************************
+# Read one byte on the SPI
+#*************************************************************************************
 def ReadSPI():
-
-    value=soc.bcm2835_spi_transfer(0x00)
-    return value
- 
+	value=soc.bcm2835_spi_transfer(0x00)
+	return value
+#*************************************************************************************
+#	ReadFIFO()
+#
+#	Overview:
+#			The function reads one byte from the FIFO
+#
+#	PreCondition:
+#			MRF89XA transciever has been properly initialized
+#
+#	input:
+#			None
+#
+#	Output:
+#			Data from FIFO
+#
+#	Side effecs:
+#			the packet has been sent out
+#*************************************************************************************			
 def ReadFIFO():
-    soc.bcm2835_gpio_write(trCSDATA,0);
-#    trCSDATA = 0;
-    value = ReadSPI();
-    soc.bcm2835_gpio_write(trCSDATA,1);
-#    trCSDATA = 1;
-    return value
-	
+	soc.bcm2835_gpio_write(trCSDATA,0);
+	value = ReadSPI();
+	soc.bcm2835_gpio_write(trCSDATA,1);
+	return value
+#*************************************************************************************
+# Send one uint8_t on the SPI
+#*************************************************************************************	
 def WriteSPI(databyte):
-
-
 	soc.bcm2835_spi_transfer(databyte)
 	soc.bcm2835_delayMicroseconds(30)	
 	return
+#*************************************************************************************
+#
+#	WriteFIFO(Data)
+#
+#	Overview:
+#			This function fills the FIFO
+#
+#	PreCondition:
+#			MRF89XA transceiver has been properly initialized
+#
+#	Input:
+#			Data - Data to be sent to FIFO
+#
+#	Output:
+#			None
+#
+#	Side effects:
+#			The packet has been sent out
+#*************************************************************************************
 def WriteFIFO(Data):
-
-    soc.bcm2835_gpio_write(trCSDATA, 0);
-#    trCSDATA = 0;
-    WriteSPI(Data);
-    soc.bcm2835_gpio_write(trCSDATA, 1);
-#    trCSDATA = 1;
-    return
-
+	soc.bcm2835_gpio_write(trCSDATA, 0);
+	WriteSPI(Data);
+	soc.bcm2835_gpio_write(trCSDATA, 1);
+	return
+#************************************************************************************
+#	RegisterRead(adress)
+#
+#	Overview:
+#			This function access the control register of MRF89XA.
+#			The register address and the register settings are
+#			input.
+#
+#	PreCondition:
+#			None
+#
+#	Input: 
+#			Data
+#
+#	Output:
+#			None
+#
+#	Side effects:
+#			Register settings have been modified
+#************************************************************************************
 def RegisterRead(adress):
-    soc.bcm2835_gpio_write(trCSCON,0);
-    #trCSCON = 0;
-    adress = ((adress<<1)|0x40);
-    WriteSPI(adress);
-    value=c_ubyte()	 
-    value = ReadSPI();
-    soc.bcm2835_gpio_write(trCSCON, 1);
-    #trCSCON = 1;
-    return value
+	soc.bcm2835_gpio_write(trCSCON,0);
+	adress = ((adress<<1)|0x40);
+	WriteSPI(adress);
+	value=c_ubyte()	 
+	value = ReadSPI();
+	soc.bcm2835_gpio_write(trCSCON, 1);
+	return value
+#************************************************************************************
+#	RegisterSet(adress, value)
+#
+#	Overview: 
+#			This function access the control of MRF89XA.
+#			The register adress and the register
+#			settings are the input.
+#
+#	 PreCondition:
+#			None
+#
+# 	Input: 
+# 			adress, value
+#
+#	Output:
+#			None
+#
+#	Side effects:
+#			Register settings have been modified
+#************************************************************************************
 def RegisterSet(adress, value):
+	soc.bcm2835_gpio_write(trCSCON,0);
+	adress = (adress<<1);
+	WriteSPI(adress);
+	WriteSPI(value);
+	soc.bcm2835_gpio_write(trCSCON,1);
+	return
 
-    soc.bcm2835_gpio_write(trCSCON,0);
-    #trCSCON = 0;
-    adress = (adress<<1);
-    WriteSPI(adress);
-    WriteSPI(value);
-    soc.bcm2835_gpio_write(trCSCON,1);
-
-#    trCSCON = 1;
-    return
 def SetRFMode(mode):
 	soc.bcm2835_gpio_write(trCSCON,0);
-#	trCSCON = 0;
 	mcparam0_read=c_ubyte()
 	mcparam0_read = RegisterRead(0x00);
 	if mode == 0x80:
@@ -93,19 +155,19 @@ def SetRFMode(mode):
 		RegisterSet(0x00, (mcparam0_read & 0x1F) | 0x00)
 		
 	soc.bcm2835_gpio_write(trCSCON,1);
-#	trCSCON = 1;
 	return
-
-
+#************************************************************************************
+# Initiate the transiver
+#************************************************************************************
 def TransiverInit():  
-	RegisterSet(0x00, 0x30);					#Set to the right frequency 
+	RegisterSet(0x00, 0x30);				#Set to the right frequency 
 	RegisterSet(0x01,(0xA8)); 				#Set transiver into buffer mode
-	RegisterSet(0x02, 0x09);	#0x09				#Set the frequency deeviation to 40KHz
-	RegisterSet(0x03, 0x07);	#0x09				#Set the bitrate to 25kbps	
+	RegisterSet(0x02, 0x09);				#Set the frequency deeviation to 40KHz
+	RegisterSet(0x03, 0x07);				#Set the bitrate to 25kbps	
 	RegisterSet(0x05,(0x0A));				#Set the FIFO-size to 16 bytes and the treshhold for interrupts to 10
-	RegisterSet(0x0D,(0x00|0xB0|0x09));	#IRQ0RX = nFIFOEmpty, IRQ1RX = FIFO_THRESHOLD, IRQ0TX = nFIFOEMPTY, IRQ1TX = TXDONE
-	RegisterSet(0x0E,(0x01|0x10|0x01));	#	#Start to till FIFO when sync word is detected, start transmission when FIFO_not_full
-	RegisterSet(0x10, 0xA3);					#FIltersetting for the transiver
+	RegisterSet(0x0D,(0x00|0xB0|0x09));			#IRQ0RX = nFIFOEmpty, IRQ1RX = FIFO_THRESHOLD, IRQ0TX = nFIFOEMPTY, IRQ1TX = TXDONE
+	RegisterSet(0x0E,(0x01|0x10|0x01));			#Start to till FIFO when sync word is detected, start transmission when FIFO_not_full
+	RegisterSet(0x10, 0xA3);				#FIltersetting for the transiver
 	RegisterSet(0x12,(0x18|0x20|0x06));		#SYNC-word enabled, 32 bit size, 3 errors allowed
 	RegisterSet(0x16,0x53);				#
 	RegisterSet(0x17,0x59);				#
@@ -128,21 +190,18 @@ def TransiverInit():
 	#Clear PLL_LOCK-flag so we can see it restore on the new frequency
 	RegisterSet(0x0E,(RegisterRead(0x0E)|0x02));
 	SetRFMode(0x00);
-	return 
+	return
+ 
 def TransmittString(dataString):
-
-	#Local variables
-	
-
 	#Initiation of transmitt sequence
-	SetRFMode(0x20);								#Transiver into Standby
-	soc.bcm2835_delay(10);										#Wait for oscillator to wake up
-	SetRFMode(0x40);							#Transiver into syntesize
-	RegisterSet(0x0E,(RegisterRead(0x0E)|0x02));#Clear the bit for detection for the PLL Lock
-	while (RegisterRead(0x0E & 0b00000010)) == 0:	#Wait for the PLL to lock
+	SetRFMode(0x20);					#Transiver into Standby
+	soc.bcm2835_delay(10);					#Wait for oscillator to wake up
+	SetRFMode(0x40);					#Transiver into syntesize
+	RegisterSet(0x0E,(RegisterRead(0x0E)|0x02));		#Clear the bit for detection for the PLL Lock
+	while (RegisterRead(0x0E & 0b00000010)) == 0:		#Wait for the PLL to lock
 		h=0
-	SetRFMode(0x80);							#Set the transiver into tranmitt mode
-	soc.bcm2835_delayMicroseconds(500);									#Transmitter wake-up time
+	SetRFMode(0x80);					#Set the transiver into tranmitt mode
+	soc.bcm2835_delayMicroseconds(500);			#Transmitter wake-up time
 
 
 	WriteFIFO(0x53);
@@ -164,142 +223,66 @@ def TransmittString(dataString):
 	soc.bcm2835_delay(1);
 	return
 
-
-
-
 def main():
-   
+ 
+	 
 	 #If you call this, it will not actually access the GPIO
-# Use for testing
-#        bcm2835_set_debug(1);
-    if not soc.bcm2835_init():
-      	print( "bcm2835_init failed. Are you running as root??")
-      	return
-    #}
-    if not soc.bcm2835_spi_begin:
-      	print ("bcm2835_spi_begin failedg. Are you running as root??")
-      	return 
-    #}    #Set the GPIO inputs and outs
-    soc.bcm2835_gpio_fsel(trCSCON, soc.BCM2835_GPIO_FSEL_OUTP)
-    soc.bcm2835_gpio_fsel(trCSDATA, soc.BCM2835_GPIO_FSEL_OUTP)
-    soc.bcm2835_gpio_fsel(trRESET, soc.BCM2835_GPIO_FSEL_OUTP)
-    soc.bcm2835_gpio_fsel(trIRQ0, soc.BCM2835_GPIO_FSEL_INPT)
-    soc.bcm2835_gpio_fsel(trIRQ1, soc.BCM2835_GPIO_FSEL_INPT)
-    # Configure the SPI comunication
-    soc.bcm2835_spi_setBitOrder(soc.BCM2835_SPI_BIT_ORDER_MSBFIRST)     # The default
-    soc.bcm2835_spi_setDataMode(soc.BCM2835_SPI_MODE0)                   # The default
-    soc.bcm2835_spi_setClockDivider(soc.BCM2835_SPI_CLOCK_DIVIDER_512)  # 512 = 488.28125kHz on Rpi2, 781.25kHz on RPI3
-    soc.bcm2835_spi_chipSelect(soc.BCM2835_SPI_CS0)                     # The default
-    soc.bcm2835_spi_setChipSelectPolarity(soc.BCM2835_SPI_CS0, 0)      # the default
-    soc.bcm2835_gpio_write(trCSCON, 1)
-    soc.bcm2835_gpio_write(trCSDATA, 1)
+	# Use for testing
+	#        bcm2835_set_debug(1);
+	if not soc.bcm2835_init():
+  		print( "bcm2835_init failed. Are you running as root??")
+  		return
 
-    soc.bcm2835_gpio_write(trRESET, 1)
-    soc.bcm2835_delayMicroseconds(100)
-    soc.bcm2835_gpio_write(trRESET, 0)
-    soc.bcm2835_delay(6)
+	if not soc.bcm2835_spi_begin:
+  		print ("bcm2835_spi_begin failedg. Are you running as root??")
+  		return 
+	#Set the GPIO inputs and outs
+	soc.bcm2835_gpio_fsel(trCSCON, soc.BCM2835_GPIO_FSEL_OUTP)
+	soc.bcm2835_gpio_fsel(trCSDATA, soc.BCM2835_GPIO_FSEL_OUTP)
+	soc.bcm2835_gpio_fsel(trRESET, soc.BCM2835_GPIO_FSEL_OUTP)
+	soc.bcm2835_gpio_fsel(trIRQ0, soc.BCM2835_GPIO_FSEL_INPT)
+	soc.bcm2835_gpio_fsel(trIRQ1, soc.BCM2835_GPIO_FSEL_INPT)
+	# Configure the SPI comunication
+	soc.bcm2835_spi_setBitOrder(soc.BCM2835_SPI_BIT_ORDER_MSBFIRST)     # The default
+	soc.bcm2835_spi_setDataMode(soc.BCM2835_SPI_MODE0)                   # The default
+	soc.bcm2835_spi_setClockDivider(soc.BCM2835_SPI_CLOCK_DIVIDER_512)  # 512 = 488.28125kHz on Rpi2, 781.25kHz on RPI3
+	soc.bcm2835_spi_chipSelect(soc.BCM2835_SPI_CS0)                     # The default
+	soc.bcm2835_spi_setChipSelectPolarity(soc.BCM2835_SPI_CS0, 0)      # the default
+	soc.bcm2835_gpio_write(trCSCON, 1)
+	soc.bcm2835_gpio_write(trCSDATA, 1)
 
-          
-    TransiverInit()
-    TransmittString("N1BLINK")	
+	soc.bcm2835_gpio_write(trRESET, 1)
+	soc.bcm2835_delayMicroseconds(100)
+	soc.bcm2835_gpio_write(trRESET, 0)
+	soc.bcm2835_delay(6)
+   
+	TransiverInit()
+	
+	advertise_service( server_sock, "SampleServer", service_id = uuid,service_classes = [ uuid, SERIAL_PORT_CLASS ],profiles = [ SERIAL_PORT_PROFILE ],)
+                   
+	print("Waiting for connection on RFCOMM channel %d" % port)
+
+	client_sock, client_info = server_sock.accept()
+	print("Accepted connection from ", client_info)
+
+	try:
+    		while True:
+       			data = client_sock.recv(1024)
+        		if len(data) == 0: break
+        		print("received [%s]" % data)
+			if data=="Flash":
+				TransmittString("N1BLINK")
+			client_sock.send("he")
+			print "sending [%s]" % data	 	
+	except IOError:
+    		pass
+
+		print("disconnected")
+
+	client_sock.close()
+	server_sock.close()
+	print("all done") 	
 if __name__ == '__main__':
     main()
-
-
-
-#**************************************************************************************
-# Send one uint8_t on the SPI
-#**************************************************************************************
-
-#**************************************************************************************
-# Read one byte on the SPI
-#******************************************
-#**************************************************************************************
-# Initiate the transiver
-#**************************************************************************************
-
-
-#*********************************************************************
-# * WORD RegisterRead()
-# *
-# * Overview:        
-# *              This function access the control register of MRF89XA.
-# *              The register address and the register settings are
-# *              the input
-# *
-# * PreCondition:    
-# *              None
-# *
-# * Input:       
-# *          WORD    setting     The address of the register and its
-# *                              corresponding settings
-# *
-# * Output:  None    
-# *
-# * Side Effects:    Register settings have been modified
-# *
-# ********************************************************************/
-
-
-#*********************************************************************
-# * void RegisterSet(uint8_t address, uint8_t value)
-# *
-# * Overview:        
-# *              This function access the control register of MRF89XA.
-# *              The register address and the register settings are
-# *              the input
-# *
-# * PreCondition:    
-# *            	 None
-# *
-# * Input:       
-# *          WORD    setting     The address of the register and its
-# *                              corresponding settings
-# *
-# * Output:  None    
-# *
-# * Side Effects:    Register settings have been modified
-# *
-# ********************************************************************/
-
-
-#/*********************************************************************
-# * uint8_t ReadFIFO(void)
-# *
-# * Overview:        
-# *              The function reads one byte from the FIFO
-# *
-# * PreCondition:    
-# *              MRF89XA transceiver has been properly initialized
-# *
-# * Input:       
-# *              None
-# *
-# * Output:      Data from FIFO
-# *
-# * Side Effects:    
-# *              The packet has been sent out
-# *
-# ********************************************************************/
-
-
-#/*********************************************************************
-# * void WriteFIFO(uint8_t Data)
-# *
-# * Overview:        
-# *              This function fills the FIFO
-# *
-# * PreCondition:    
-# *              MRF89XA transceiver has been properly initialized
-# *
-# * Input:       
-# *              uint8_t   Data - Data to be sent to FIFO.
-# *
-# * Output:      None
-# *
-# * Side Effects:    
-# *              The packet has been sent out
-# *
-# ********************************************************************/
 
 
